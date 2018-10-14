5c3f3ce7d72d1a277a13a46bdf56a52f2596fa7e
svetoslavmishev <svetlioo.m@gmail.com>
2018-10-14 13:58:01 +0300

    init commit

--- Redux/src/calculator/actionTypes.js
+++
@@ -1,4 +0,0 @@
-export const ADD = 'ADD';
-export const SUBTRACT = 'SUBTRACT';
-export const MULTIPLY = 'MULTIPLY';
-export const DIVIDE = 'DIVIDE';
--- Redux/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import calculatorApp from './calculator/calculatorApp';
-
-ReactDOM.render(<App />, document.getElementById('root'));
